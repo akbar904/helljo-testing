cd23d9d54812a0a37dd721205a678a296a249911
zharfan-a <zharfan@walturn.com>
2024-09-22 22:20:18 +0700

    Update multiple files

---
+++ workspace/src/lib/models/animal_model.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class Animal {
+	final String name;
+	final IconData icon;
+
+	Animal({required this.name, required this.icon});
+
+	Map<String, dynamic> toJson() => {
+		'name': name,
+		'icon': icon.codePoint,
+	};
+
+	factory Animal.fromJson(Map<String, dynamic> json) {
+		return Animal(
+			name: json['name'],
+			icon: IconData(json['icon'], fontFamily: 'MaterialIcons'),
+		);
+	}
+}
